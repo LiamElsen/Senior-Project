f57b6291b16331279a653d371ccfc5f0b48a66f3
Liam Elsen <liamelsen64@gmail.com>
2019-04-25 17:03:15 -0400

    continuing to build gaming pc function

--- JS/function-library.js
+++ JS/function-library.js
@@ -6,14 +6,38 @@ function buildGamingPc(formData) {
             formData["streamingQuestion"] == 1
         ) {
             let resultObject = {
-                'imageLink': '',
-                'externalUrl': ''
+                'imageLink': 'Images/tier1-gaming-desktop.jpg',
+                'externalUrl': 'https://www.amazon.com/Gaming-Desktop-Computer-3-20GHz-Windows/dp/B07K3TM9W5/ref=sr_1_1?keywords=gaming+desktop&qid=1556224474&refinements=p_n_condition-type%3A6461716011&rnid=6461714011&s=gateway&sr=8-1'
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["gameQuestion"] == 'Minecraft' &&
+            formData["resolutionQuestion"] == 1 &&
+            formData["streamingQuestion"] == 1
+        ) {
+            let resultObject = {
+                'imageLink': 'Images/tier1-gaming-desktop.jpg',
+                'externalUrl': 'https://www.amazon.com/Gaming-Desktop-Computer-3-20GHz-Windows/dp/B07K3TM9W5/ref=sr_1_1?keywords=gaming+desktop&qid=1556224474&refinements=p_n_condition-type%3A6461716011&rnid=6461714011&s=gateway&sr=8-1'
             }
 
             return resultObject;
         }
     } else {
-        console.log('Laptop Selected');
+        if (
+            formData["gameQuestion"] == 'Minecraft' &&
+            formData["resolutionQuestion"] == 1 &&
+            formData["streamingQuestion"] == 1
+        ) {
+            let resultObject = {
+                'imageLink': 'Images/tier1-gaming-desktop.jpg',
+                'externalUrl': 'https://www.amazon.com/Gaming-Desktop-Computer-3-20GHz-Windows/dp/B07K3TM9W5/ref=sr_1_1?keywords=gaming+desktop&qid=1556224474&refinements=p_n_condition-type%3A6461716011&rnid=6461714011&s=gateway&sr=8-1'
+            }
+
+            return resultObject;
+        }
     } 
 
     let result;
