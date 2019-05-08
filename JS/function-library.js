f5d697b8db1475a9b43270205588777ae3573987
Liam Elsen <liamelsen64@gmail.com>
2019-05-08 09:12:11 -0400

    Finished Questions Began Entering Links

--- JS/function-library.js
+++ JS/function-library.js
@@ -320,3 +320,650 @@ function buildGamingPc(formData) {
     let result;
     return result;
 }
+
+
+
+
+
+
+
+
+
+function buildProductivityPc(formData) {
+    if (formData["lapDeskQuestion"] == 'Desktop') {
+        if (
+            formData["programQuestion"] == 'Microsoft Office' &&
+            formData["webcamQuestion"] == 'Yes' &&
+            formData["storageQuestion"] == '256GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'Microsoft Office' &&
+            formData["webcamQuestion"] == 'Yes' &&
+            formData["storageQuestion"] == '512GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'Microsoft Office' &&
+            formData["webcamQuestion"] == 'Yes' &&
+            formData["storageQuestion"] == '1TB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+
+        if (
+            formData["programQuestion"] == 'Microsoft Office' &&
+            formData["webcamQuestion"] == 'No' &&
+            formData["storageQuestion"] == '256GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'Microsoft Office' &&
+            formData["webcamQuestion"] == 'No' &&
+            formData["storageQuestion"] == '512GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'Microsoft Office' &&
+            formData["webcamQuestion"] == 'No' &&
+            formData["storageQuestion"] == '1TB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'CAD Software' &&
+            formData["webcamQuestion"] == 'Yes' &&
+            formData["storageQuestion"] == '256GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'CAD Software' &&
+            formData["webcamQuestion"] == 'Yes' &&
+            formData["storageQuestion"] == '512GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'CAD Software' &&
+            formData["webcamQuestion"] == 'Yes' &&
+            formData["storageQuestion"] == '1TB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+
+        if (
+            formData["programQuestion"] == 'CAD Software' &&
+            formData["webcamQuestion"] == 'No' &&
+            formData["storageQuestion"] == '256GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'CAD Software' &&
+            formData["webcamQuestion"] == 'No' &&
+            formData["storageQuestion"] == '512GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'CAD Software' &&
+            formData["webcamQuestion"] == 'No' &&
+            formData["storageQuestion"] == '1TB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'Scientific WorkPlace' &&
+            formData["webcamQuestion"] == 'Yes' &&
+            formData["storageQuestion"] == '256GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'Scientific WorkPlace' &&
+            formData["webcamQuestion"] == 'Yes' &&
+            formData["storageQuestion"] == '512GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'Scientific WorkPlace' &&
+            formData["webcamQuestion"] == 'Yes' &&
+            formData["storageQuestion"] == '1TB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+
+        if (
+            formData["programQuestion"] == 'Scientific WorkPlace' &&
+            formData["webcamQuestion"] == 'No' &&
+            formData["storageQuestion"] == '256GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'Scientific WorkPlace' &&
+            formData["webcamQuestion"] == 'No' &&
+            formData["storageQuestion"] == '512GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'Scientific WorkPlace' &&
+            formData["webcamQuestion"] == 'No' &&
+            formData["storageQuestion"] == '1TB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+       
+    } else {
+        if (
+            formData["programQuestion"] == 'Microsoft Office' &&
+            formData["webcamQuestion"] == 'Yes' &&
+            formData["storageQuestion"] == '256GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'Microsoft Office' &&
+            formData["webcamQuestion"] == 'Yes' &&
+            formData["storageQuestion"] == '512GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'Microsoft Office' &&
+            formData["webcamQuestion"] == 'Yes' &&
+            formData["storageQuestion"] == '1TB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+
+        if (
+            formData["programQuestion"] == 'Microsoft Office' &&
+            formData["webcamQuestion"] == 'No' &&
+            formData["storageQuestion"] == '256GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'Microsoft Office' &&
+            formData["webcamQuestion"] == 'No' &&
+            formData["storageQuestion"] == '512GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'Microsoft Office' &&
+            formData["webcamQuestion"] == 'No' &&
+            formData["storageQuestion"] == '1TB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'CAD Software' &&
+            formData["webcamQuestion"] == 'Yes' &&
+            formData["storageQuestion"] == '256GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'CAD Software' &&
+            formData["webcamQuestion"] == 'Yes' &&
+            formData["storageQuestion"] == '512GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'CAD Software' &&
+            formData["webcamQuestion"] == 'Yes' &&
+            formData["storageQuestion"] == '1TB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+
+        if (
+            formData["programQuestion"] == 'CAD Software' &&
+            formData["webcamQuestion"] == 'No' &&
+            formData["storageQuestion"] == '256GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'CAD Software' &&
+            formData["webcamQuestion"] == 'No' &&
+            formData["storageQuestion"] == '512GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'CAD Software' &&
+            formData["webcamQuestion"] == 'No' &&
+            formData["storageQuestion"] == '1TB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'Scientific WorkPlace' &&
+            formData["webcamQuestion"] == 'Yes' &&
+            formData["storageQuestion"] == '256GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'Scientific WorkPlace' &&
+            formData["webcamQuestion"] == 'Yes' &&
+            formData["storageQuestion"] == '512GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'Scientific WorkPlace' &&
+            formData["webcamQuestion"] == 'Yes' &&
+            formData["storageQuestion"] == '1TB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+
+        if (
+            formData["programQuestion"] == 'Scientific WorkPlace' &&
+            formData["webcamQuestion"] == 'No' &&
+            formData["storageQuestion"] == '256GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'Scientific WorkPlace' &&
+            formData["webcamQuestion"] == 'No' &&
+            formData["storageQuestion"] == '512GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["programQuestion"] == 'Scientific WorkPlace' &&
+            formData["webcamQuestion"] == 'No' &&
+            formData["storageQuestion"] == '1TB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+
+
+
+
+
+
+function buildWebBrowsingPc(formData) {
+    if (formData["lapDeskQuestion"] == 'Desktop') {
+        if (
+            formData["webcamQuestion"] == 'No' &&
+            formData["storageQuestion"] == '256GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["webcamQuestion"] == 'No' &&
+            formData["storageQuestion"] == '512GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["webcamQuestion"] == 'No' &&
+            formData["storageQuestion"] == '1TB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+
+        if (
+            formData["webcamQuestion"] == 'Yes' &&
+            formData["storageQuestion"] == '256GB'
+        ) {
+            let resultObject = {
+                'imageLink': 'Images/.jpg',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["webcamQuestion"] == 'Yes' &&
+            formData["storageQuestion"] == '512GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["webcamQuestion"] == 'Yes' &&
+            formData["storageQuestion"] == '1TB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+       
+    } else {
+        if (
+            formData["webcamQuestion"] == 'No' &&
+            formData["storageQuestion"] == '256GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': 'https://www.microsoft.com/en-us/store/config/surface-laptop-2/8xqjkk3dd91b/jx8c'
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["webcamQuestion"] == 'No' &&
+            formData["storageQuestion"] == '512GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': 'https://www.microsoft.com/en-us/store/config/surface-laptop-2/8xqjkk3dd91b/jx8c'
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["webcamQuestion"] == 'No' &&
+            formData["storageQuestion"] == '1TB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': 'https://www.microsoft.com/en-us/store/config/surface-laptop-2/8xqjkk3dd91b/jx8c'
+            }
+
+            return resultObject;
+        }
+
+
+        if (
+            formData["webcamQuestion"] == 'Yes' &&
+            formData["storageQuestion"] == '256GB'
+        ) {
+            let resultObject = {
+                'imageLink': 'Images/.jpg',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["webcamQuestion"] == 'Yes' &&
+            formData["storageQuestion"] == '512GB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+
+        if (
+            formData["webcamQuestion"] == 'Yes' &&
+            formData["storageQuestion"] == '1TB'
+        ) {
+            let resultObject = {
+                'imageLink': '',
+                'externalUrl': ''
+            }
+
+            return resultObject;
+        }
+    } 
+
+    let result;
+    return result;
+}
