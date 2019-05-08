ff86737a9063c9585a2a1524e598b1487bd09ae6
Liam Elsen <liamelsen64@gmail.com>
2019-05-08 15:38:40 -0400

    Questions done with more links entered

--- JS/function-library.js
+++ JS/function-library.js
@@ -6,7 +6,7 @@ function buildGamingPc(formData) {
             formData["streamingQuestion"] == 1
         ) {
             let resultObject = {
-                'imageLink': 'Images/tier1-gaming-desktop.jpg',
+                'imageLink': 'Images/81vuQbgokgL._SL1500_.jpg',
                 'externalUrl': 'https://www.amazon.com/Gaming-Desktop-Computer-3-20GHz-Windows/dp/B07K3TM9W5/ref=sr_1_1?keywords=gaming+desktop&qid=1556224474&refinements=p_n_condition-type%3A6461716011&rnid=6461714011&s=gateway&sr=8-1'
             }
 
@@ -807,8 +807,8 @@ function buildProductivityPc(formData) {
             return resultObject;
         }
 
-
-
+    }
+}
 
 
 
